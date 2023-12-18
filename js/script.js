7fe2399b2bd5e9317cbf0a6f5adaf1f5f6a80cc7
fungky26 <fungkysetiawan@gmail.com>
2023-12-19 02:09:21 +0700

    update sukses jss

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-function replacename(){
-let number = prompt("masukkan nama anda","");
-document.getElementById("nama").innerHTML = nama
+function submitForm(event) {
+    event.preventDefault(); // Mencegah formulir untuk melakukan submit
+
+    // Mengambil nilai dari input nama
+    var nameInput = document.getElementById('nama-input');
+    var name = nameInput.value;
+
+    // Memeriksa apakah input nama tidak kosong
+    if (name.trim() !== '') {
+        // Mengganti teks di elemen dengan id 'name' dengan nama yang dimasukkan
+        document.getElementById('name').textContent = name;
+
+        // Mengosongkan input nama dan pesan kesalahan (jika ada)
+        nameInput.value = '';
+        document.getElementById('error-name').textContent = '';
+    } else {
+        // Menampilkan pesan kesalahan jika input nama kosong
+        document.getElementById('error-name').textContent = 'Nama tidak boleh kosong';
+    }
 }
